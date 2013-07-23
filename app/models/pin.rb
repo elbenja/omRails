f7ba38f2f0729cb239ff2e58865786a3e64f1e99
benjamin saravia <elbenja@gmail.com>
2013-07-23 18:05:34 -0400

    paperclip imp

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,16 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :image # This line won't work in Rails 4, instead, you'll have to do what I show below
+
   validates :description, presence: true
+  validates :user_id, presence: true
+  has_attached_file :image, styles: { medium: "320x240>"}
+
+	validates_attachment :image, presence: true,
+                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                            size: { less_than: 5.megabytes }               
+
+# validates_attachment_content_type :image, :content_type=>['image/jpeg', 'image/png', 'image/gif'] 
 
   belongs_to :user
-  validates :user_id, presence: true
+
 end
