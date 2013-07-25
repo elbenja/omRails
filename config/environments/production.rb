0bc2f9a2be297801e3b73eace693648002129ef7
benjamin saravia <elbenja@gmail.com>
2013-07-25 09:36:24 -0400

    amazon S3 3rd try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,16 @@ Omrails::Application.configure do
 
   # will change domain
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
 
-# config/environments/production.rb
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
