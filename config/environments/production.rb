75ab453cbf72421a261b0ea45131a61b0a2de17b
benjamin saravia <elbenja@gmail.com>
2013-07-25 09:18:15 -0400

    Add Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,14 @@ Omrails::Application.configure do
 
   # will change domain
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
-
 end
+
+# config/environments/production.rb
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
