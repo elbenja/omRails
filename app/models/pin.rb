5b9dda99ff4c988fb7c14b2e201f018afdd1586f
benjamin saravia <elbenja@gmail.com>
2013-07-25 11:25:36 -0400

    remote url image

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,8 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image # This line won't work in Rails 4, instead, you'll have to do what I show below
-
+  attr_accessible :description, :image , :image_remote_url
   validates :description, presence: true
   validates :user_id, presence: true
-  has_attached_file :image, styles: { medium: "320x840>"}
+  has_attached_file :image, styles: { medium: "320x1220>"}
 
 	validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
@@ -12,5 +11,9 @@ class Pin < ActiveRecord::Base
 # validates_attachment_content_type :image, :content_type=>['image/jpeg', 'image/png', 'image/gif'] 
 
   belongs_to :user
-
+  
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  	super
+  end
 end
